4bbfee184b9661b700bd5528ebe3532f00401b14
dantheman8300 <daniel.7039@hotmail.com>
2023-06-26 14:21:16 -0700

    home page robust

--- minting-site/src/pages/index.tsx
+++ minting-site/src/pages/index.tsx
@@ -7,15 +7,15 @@ export default function Home() {
 return (
     <div className="bg-base-100 h-screen overflow-hidden">
         <Header 
-            title="Collection name"
+            title={process.env.COLLECTION_NAME || "Collection Name"}
             showSocials={true}
         />
         <div className="hero h-full">
             <div className="hero-content flex-col lg:flex-row">
                 <Image src={landingPageHeroPic} alt="landing page hero banner" className="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 className="text-5xl font-bold">Collection website</h1>
-                    <p className="py-6">This is your very own NFT minting website! Follow along with Overmind's guide on how to create your own collection and set up your minting site! Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="text-5xl font-bold">{process.env.COLLECTION_NAME || "Collection Name"} Website</h1>
+                    <p className="py-6">{process.env.COLLECTION_DESCRIPTION}</p>
                     <a href="/mint"><button className="btn btn-primary">Mint!</button></a>
                 </div>
             </div>
